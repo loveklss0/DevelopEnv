ded541e6ce8bb082f40abde9ef4c0e5e94a8a08b
huqiang1 <huqiang1@letv.com>
2016-10-12 19:30:17 +0800

    add .oh-my-zsh

---
+++ .oh-my-zsh/plugins/fasd/fasd.plugin.zsh
@@ -0,0 +1,20 @@
+if [ $commands[fasd] ]; then # check if fasd is installed
+  fasd_cache="${ZSH_CACHE_DIR}/fasd-init-cache"
+  if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+    fasd --init auto >| "$fasd_cache"
+  fi
+  source "$fasd_cache"
+  unset fasd_cache
+
+  alias v="f -e vim"
+  alias vv="f -e -i vim"
+  alias o='a -e open_command'
+  alias a='fasd -a'        # any
+  alias s='fasd -si'       # show / search / select
+  alias d='fasd -d'        # directory
+  alias f='fasd -f'        # file
+  alias sd='fasd -sid'     # interactive directory selection
+  alias sf='fasd -sif'     # interactive file selection
+  alias j='fasd_cd -d'     # cd, same functionality as j in autojump
+  alias jj='fasd_cd -d -i' # cd with interactive selection
+fi
